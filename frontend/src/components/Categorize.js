e52794fb9e6bbd443d4d16a51fd3dfb3f427bd32
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-08-02 12:02:34 +0530

    delete functionality is remaining

--- frontend/src/components/Categorize.js
+++ frontend/src/components/Categorize.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 
 function Categorize() {
   const [isHidden, setIsHidden] = useState("hidden");
-  const [catgory, setCatgory] = useState([{ Category: 1 }]);
+  const [category, setCategory] = useState([{ id: 1, val: "" }]);
+  const [inp, setInpu] = useState("");
+  console.log(category);
+
   return (
     <div className="flex flex-col  items-center">
       <h1 className="text-6xl font-bold">Categorize</h1>
 
       <form className=" flex justify-center border-solid border-2 mt-10 w-1/2 font-sans shadow-2xl rounded-2xl">
-        <div className="grid grid-cols-3 place-content-start grid-rows-4 gap-6 w-11/12 my-5">
+        <div className="grid grid-cols-3 place-content-start grid-rows-4 gap-1 w-11/12 my-5">
           <input
             type="text"
             className="col-span-full px-5 border-solid border-gray-200 border-2 rounded-md"
@@ -23,39 +26,60 @@ function Categorize() {
 
           <div className="col-span-full flex flex-col">
             <p className=" font-medium">Categories</p>
-            {catgory.map((el, idx) => {
+            {category.map((el, idx) => {
               return (
-                <>
-                  <div key={idx} className="flex items-center">
-                    <span
-                      className="material-symbols-outlined"
-                      style={{ visibility: isHidden }}
-                    >
-                      drag_indicator
-                      {console.log(isHidden)}
-                    </span>
-                    <label htmlFor="category" className="">
-                      <input
-                        type="text"
-                        className="border-solid  px-3 border-gray-200 border-2 rounded-md"
-                        placeholder={`Category ${1}`}
-                        onFocus={() => {
-                          setIsHidden("");
-                          console.log(isHidden);
-                        }}
-                        onBlur={() => {
-                          setIsHidden("hidden");
-                        }}
-                      />
-                    </label>
-                    <span
-                      className="material-symbols-outlined"
-                      style={{ visibility: isHidden }}
-                    >
-                      close
-                    </span>
-                  </div>
-                </>
+                <div key={idx} className="flex items-center">
+                  <span
+                    className="material-symbols-outlined"
+                    style={{ visibility: isHidden }}
+                  >
+                    drag_indicator
+                    {console.log(isHidden)}
+                  </span>
+                  <label htmlFor="category" className="">
+                    <input
+                      type="text"
+                      className="border-solid  px-3 border-gray-200 border-2 rounded-md"
+                      placeholder={`Category ${el.id}`}
+                      onFocus={() => {
+                        setIsHidden("");
+                      }}
+                      onBlur={() => {
+                        setIsHidden("hidden");
+                      }}
+                      onChange={(e) => {
+                        setInpu(e.target.value);
+                      }}
+                      onKeyPress={(e) => {
+                        if (e.target.value !== "") {
+                          if (e.key === "Enter") {
+                            console.log("key pressed", e, category);
+
+                            setCategory([
+                              ...category,
+                              {
+                                id: el.id + 1,
+                                val: inp,
+                              },
+                            ]);
+                          }
+                        }
+                      }}
+                    />
+                  </label>
+                  <span
+                    className="material-symbols-outlined"
+                    style={{ visibility: isHidden }}
+                    onClick={() => {
+                      const updatedCategory = category.filter((e, ind) => {
+                        return e.target.id !== ind;
+                      });
+                      setCategory(updatedCategory);
+                    }}
+                  >
+                    close
+                  </span>
+                </div>
               );
             })}
           </div>
