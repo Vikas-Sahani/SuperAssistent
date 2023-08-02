b13bdb055e6b04be0253c6f347e58a5cb52231d2
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-08-02 16:46:52 +0530

    drag & drop functionality is remaining

--- frontend/src/components/Categorize.js
+++ frontend/src/components/Categorize.js
@@ -1,45 +1,47 @@
 import React, { useState } from "react";
 
 function Categorize() {
-  const [isHidden, setIsHidden] = useState("hidden");
+  const [isHidden, setIsHidden] = useState(false);
   const [category, setCategory] = useState([{ id: 1, val: "" }]);
+  const [item, setItem] = useState([{ id: 1, val: "" }]);
   const [inp, setInpu] = useState("");
-  console.log(category);
 
   return (
     <div className="flex flex-col  items-center">
       <h1 className="text-6xl font-bold">Categorize</h1>
 
       <form className=" flex justify-center border-solid border-2 mt-10 w-1/2 font-sans shadow-2xl rounded-2xl">
-        <div className="grid grid-cols-3 place-content-start grid-rows-4 gap-1 w-11/12 my-5">
+        <div className="grid grid-cols-3 place-content-start gap-2 w-11/12 my-5">
           <input
             type="text"
-            className="col-span-full px-5 border-solid border-gray-200 border-2 rounded-md"
+            className="col-span-full px-5 my-2 border-solid border-gray-200 border-2 rounded-md"
             placeholder="Description Text"
           />
-          <div className="flex items-center col-span-full ">
+          <div className="flex items-center col-span-full my-2">
             <p className="font-medium">Medium</p>
             <select className="mx-10  px-3 border-2 border-gray-300 rounded-md text-gray-400">
               <option>None</option>
             </select>
           </div>
 
-          <div className="col-span-full flex flex-col">
+          <div className="col-span-full flex flex-col my-2">
             <p className=" font-medium">Categories</p>
             {category.map((el, idx) => {
               return (
-                <div key={idx} className="flex items-center">
+                <div key={idx} className="flex items-center my-1">
                   <span
                     className="material-symbols-outlined"
-                    style={{ visibility: isHidden }}
+                    style={{ visibility: isHidden ? "hidden" : "" }}
+                    onClick={() => {
+                      console.log("clicked");
+                    }}
                   >
                     drag_indicator
-                    {console.log(isHidden)}
                   </span>
                   <label htmlFor="category" className="">
                     <input
                       type="text"
-                      className="border-solid  px-3 border-gray-200 border-2 rounded-md"
+                      className="border-solid w-32 px-3 border-gray-200 border-2 rounded-md"
                       placeholder={`Category ${el.id}`}
                       onFocus={() => {
                         setIsHidden("");
@@ -69,13 +71,7 @@ function Categorize() {
                   </label>
                   <span
                     className="material-symbols-outlined"
-                    style={{ visibility: isHidden }}
-                    onClick={() => {
-                      const updatedCategory = category.filter((e, ind) => {
-                        return e.target.id !== ind;
-                      });
-                      setCategory(updatedCategory);
-                    }}
+                    style={{ visibility: isHidden ? "hidden" : "" }}
                   >
                     close
                   </span>
@@ -84,29 +80,78 @@ function Categorize() {
             })}
           </div>
 
-          <div className=" w-[330%] full  flex justify-between">
-            <div>
-              <p className="font-medium">Items</p>
-              <label>
-                <input
-                  type="text"
-                  className="border-solid  px-3 border-gray-200 border-2 rounded-md"
-                  placeholder={`Item ${1}`}
-                />
-              </label>
+          <div className="col-span-full grid grid-flow-col justify-between grid-cols-2 my-2">
+            <div className="flex flex-col">
+              <p className=" font-medium">Items</p>
+              {item.map((el, idx) => {
+                return (
+                  <div key={idx} className="flex items-center my-1">
+                    <span
+                      className="material-symbols-outlined"
+                      style={{ visibility: isHidden ? "hidden" : "" }}
+                    >
+                      drag_indicator
+                    </span>
+                    <label htmlFor="category" className="">
+                      <input
+                        type="text"
+                        className="border-solid w-36 px-3 border-gray-200 border-2 rounded-md"
+                        placeholder={`Item ${el.id}`}
+                        onFocus={() => {
+                          setIsHidden("");
+                        }}
+                        onBlur={() => {
+                          setIsHidden("hidden");
+                        }}
+                        onChange={(e) => {
+                          setInpu(e.target.value);
+                        }}
+                        onKeyPress={(e) => {
+                          if (e.target.value !== "") {
+                            if (e.key === "Enter") {
+                              console.log("key pressed", e, category);
+
+                              setItem([
+                                ...item,
+                                {
+                                  id: el.id + 1,
+                                  val: inp,
+                                },
+                              ]);
+                            }
+                          }
+                        }}
+                      />
+                    </label>
+                    <span
+                      className="material-symbols-outlined"
+                      style={{ visibility: isHidden ? "hidden" : "" }}
+                    >
+                      close
+                    </span>
+                  </div>
+                );
+              })}
             </div>
 
-            <div className="">
-              <p className="font-medium px-20">Belongs to</p>
-              <label>
-                <select
-                  type="text"
-                  className=" mx-10  px-3 border-2 border-gray-300 rounded-md text-gray-400"
-                  placeholder={``}
-                >
-                  <option>Choose Category</option>
-                </select>
-              </label>
+            <div className="flex flex-col">
+              <p className="font-medium mx-5">Belongs to</p>
+              {item.map((el, keyId) => {
+                return (
+                  <label key={keyId}>
+                    <select
+                      type="text"
+                      className="h-[26px] mx-5 w-42 px-3 border-2 border-gray-300 rounded-md text-gray-400 my-1"
+                      placeholder={``}
+                    >
+                      <option>Choose Category</option>
+                      {category.map((el, keyId) => {
+                        return <option key={keyId}>{el.val}</option>;
+                      })}
+                    </select>
+                  </label>
+                );
+              })}
             </div>
           </div>
         </div>
